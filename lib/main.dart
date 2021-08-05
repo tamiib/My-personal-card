d97d6bb8a7806746a8af37f43df8513c76f762ff
tamiib <tamibarisic28@gmail.com>
2021-08-05 16:49:18 +0200

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('image/tami.jpg'),
+              ),
+              Text(
+                'Tamara Barišić  ',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  //fontStyle: FontStyle.italic,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER  ',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  //fontFamily: '',
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 25.0,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      ' +35 99 282 7455',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans  Pro',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email_sharp,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'tamibarisic28@gmail.com',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
